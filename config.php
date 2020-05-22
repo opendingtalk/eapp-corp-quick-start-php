dc1a534d1c8ea0d82083b331efe0c11af24add9e
opendingtalk <31611459+opendingtalk@users.noreply.github.com>
2020-05-22 15:13:34 +0800

    Update config.php
    update config.php

--- config.php
+++ config.php
@@ -1,6 +1,8 @@
 <?php
 define('DIR_ROOT', dirname(__FILE__).'/');
 define("OAPI_HOST", "https://oapi.dingtalk.com");
-//Suite
+
+//开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppKey
 define("APP_KEY", "");
+//开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppSecret
 define("APP_SECRET", "");
